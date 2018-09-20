f9996060a1ca458e0dfd03de4e823c1664ff86e6
csli <csli2190@gmail.com>
2018-09-20 14:03:26 +0800

    add practice/cs3-2_number_classification_with_cnn

---
+++ practice/cs3-2_number_classification_with_cnn/README.md
@@ -0,0 +1,16 @@
+﻿### How to run
+```
+$ python cs3-2_number_classification_with_cnn.py
+```
+
+### Description
+```
+Identify (classify) test-images of mnist.
+Trained by using train-images of mnist with cnn.
+```
+
+* Softmax function
+	* [https://en.wikipedia.org/wiki/Softmax_function](https://en.wikipedia.org/wiki/Softmax_function)
+
+* Cross entropy
+	* [https://en.wikipedia.org/wiki/Cross_entropy](https://en.wikipedia.org/wiki/Cross_entropy)
--- practice/cs3_number_classification/cs3_number_classification.py
+++ practice/cs3_number_classification/cs3_number_classification.py
@@ -17,26 +17,35 @@ y = tf.nn.softmax(tf.matmul(x, W) + b)
 y_ = tf.placeholder(tf.float32, [None, 10])
 
 # loss, cross entropy
-cross_entropy = tf.reduce_mean( -tf.reduce_sum(y_ * tf.log(y)) )
+#cross_entropy = tf.reduce_mean( -tf.reduce_sum(y_ * tf.log(y)) )
+cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
-# # train operation
-train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+# train operation
+#train_op = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+train_op = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
+# accuracy
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+        
 # session
 sess = tf.InteractiveSession()
-
 # init variables
 tf.global_variables_initializer().run()
 
-for step in range(4000 + 1):
+for step in range(4001):
     # 100 images for a batch within mnist.train
-    # images : batch_xs shape(100, 784), labels : batch_ys shape(100, 10)
-    batch_xs, batch_ys = mnist.train.next_batch(100)
+    # images : batch[0] shape(100, 784), labels : batch[1] shape(100, 10)
+    batch = mnist.train.next_batch(100)
     
-    sess.run(train_op, feed_dict={x: batch_xs, y_: batch_ys})
+    sess.run(train_op, feed_dict={x: batch[0], y_: batch[1]})
 
     # show learning process and accuracy
     if step % 400 == 0:
-        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-        print(step, sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+        train_accuracy = sess.run(accuracy, feed_dict={x: batch[0], y_: batch[1]})
+        print("step %d, mnist.train.images(100) accuracy %g" % (step, train_accuracy))
+        
+# show accuracy using mnist.test.images
+test_batch = mnist.test.next_batch(100)
+print("final, mnist.test.images(100) accuracy %g" % sess.run(accuracy, feed_dict={x: test_batch[0], y_: test_batch[1]}))
+print("final, mnist.test.images accuracy %g" % sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
